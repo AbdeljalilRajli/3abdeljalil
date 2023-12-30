c27913f6ef464c2301a3bde06245d270a32bb509
Abdeljalil Rajli <rajli.contact@gmail.com>
2023-12-30 20:25:49 +0100

    Available text color

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "65c678f4d39f0995"; }
+/******/ 		__webpack_require__.h = function() { return "c94c8d50e08985e2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
