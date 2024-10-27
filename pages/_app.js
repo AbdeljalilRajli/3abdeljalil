8dc63c726a772bc91c1e5bbf201f67af88b76bd4
Abdeljalil Rajli <rajli.contact@gmail.com>
2024-10-27 18:49:44 +0100

    openGraph image

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,11 @@
 import PreLoader from "@/src/layout/PreLoader";
-
 import Head from "next/head";
 import { Fragment, useEffect, useState } from "react";
-import { SpeedInsights } from "@vercel/speed-insights/next"
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function App({ Component, pageProps }) {
   const [loaded, setLoaded] = useState(false);
+  
   useEffect(() => {
     setTimeout(() => {
       setLoaded(true);
@@ -14,11 +13,10 @@ export default function App({ Component, pageProps }) {
   }, []);
 
   return (
-    
     <Fragment>
       <Head>
         <meta charSet="utf-8" />
-        <meta name="description" content />
+        <meta name="description" content="Abdeljalil Rajli - Full Stack Developer" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta
           name="viewport"
@@ -26,37 +24,46 @@ export default function App({ Component, pageProps }) {
         />
         {/* Title */}
         <title>Abdeljalil Rajli - Full Stack Developer</title>
+
+        {/* Open Graph Meta Tags */}
+        <meta property="og:title" content="Abdeljalil Rajli - Full Stack Developer" />
+        <meta property="og:description" content="Explore my portfolio as a skilled full stack developer with experience in creating dynamic and responsive web applications." />
+        <meta property="og:image" content="/assets/images/website/abdeljalil-website-mockup.webp" />
+        <meta property="og:url" content="https://www.3abdeljalil.com/" />
+        <meta property="og:type" content="website" />
+        
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Abdeljalil Rajli - Full Stack Developer" />
+        <meta name="twitter:description" content="Explore my portfolio as a skilled full stack developer with experience in creating dynamic and responsive web applications." />
+        <meta name="twitter:image" content="/assets/images/website/abdeljalil-website-mockup.webp" /> {/* Same as OG image */}
+
         {/* Favicon Icon */}
         <link
           rel="shortcut icon"
           href="assets/images/favicon.png"
           type="image/x-icon"
         />
+        
         {/* Google Fonts */}
         <link
           href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap"
           rel="stylesheet"
         />
-        {/* Flaticon */}
+        
+        {/* CSS Files */}
         <link rel="stylesheet" href="assets/css/flaticon.min.css" />
-        {/* Font Awesome */}
         <link rel="stylesheet" href="assets/css/fontawesome-5.14.0.min.css" />
-        {/* Bootstrap */}
         <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
-        {/* Magnific Popup */}
         <link rel="stylesheet" href="assets/css/magnific-popup.min.css" />
-        {/* Nice Select */}
         <link rel="stylesheet" href="assets/css/nice-select.min.css" />
-        {/* Animate */}
         <link rel="stylesheet" href="assets/css/animate.min.css" />
-        {/* Slick */}
         <link rel="stylesheet" href="assets/css/slick.min.css" />
-        {/* Main Style */}
         <link rel="stylesheet" href="assets/css/style.css" />
       </Head>
-      {/* {!loaded && <PreLoader />}  */}
+      
       {loaded && <Component {...pageProps} />}
-    <SpeedInsights/>
+      <SpeedInsights />
     </Fragment>
   );
 }
