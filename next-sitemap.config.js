57462578b7bcbae3aa5ef9106ffaf9e4db276eae
Abdeljalil Rajli <rajli.contact@gmail.com>
2025-07-19 22:22:12 +0100

    Add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://www.3bdeljalil.com', // Replace with your actual domain
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
+  };
+  
